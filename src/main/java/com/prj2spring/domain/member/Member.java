2fc35e0ab58ac4e39067ee660e27ceaa6ee0fb39
jeongY <zoe1126@naver.com>
2024-05-22 15:19:03 +0900

    회원 목록 보기

--- src/main/java/com/prj2spring/domain/member/Member.java
+++ src/main/java/com/prj2spring/domain/member/Member.java
@@ -2,9 +2,21 @@ package com.prj2spring.domain.member;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Data
 public class Member {
+    private int id;
     private String email;
     private String password;
     private String nickName;
+    private LocalDateTime inserted;
+
+    public String getSignupDateTime() {
+        DateTimeFormatter formatter
+                = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 hh시 mm분 ss초");
+
+        return inserted.format(formatter);
+    }
 }
