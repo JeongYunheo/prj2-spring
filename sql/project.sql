008240f3c23e416b453ed564dabac2035d78a87a
jeongY <zoe1126@naver.com>
2024-05-27 15:08:35 +0900

    검색, 조회용 업데이트

--- sql/project.sql
+++ sql/project.sql
@@ -57,3 +57,33 @@ FROM board;
 
 SELECT COUNT(*)
 FROM board;
+
+SELECT *
+FROM member;
+
+UPDATE member
+SET nick_name='abcd'
+WHERE id = 21;
+UPDATE member
+SET nick_name='efg'
+WHERE id = 22;
+
+UPDATE board
+SET member_id = 21
+WHERE id % 2 = 0;
+UPDATE board
+SET member_id=22
+WHERE id % 2 = 1;
+
+UPDATE board
+SET title  = 'abc def',
+    content= 'ghi jkl'
+WHERE id % 3 = 0;
+UPDATE board
+SET title  = 'mno pqr',
+    content= 'stu vwx'
+WHERE id % 3 = 1;
+UPDATE board
+SET title  = 'yz1 234',
+    content= '567 890'
+WHERE id % 3 = 2;
