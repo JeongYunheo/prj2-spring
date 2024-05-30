74d106f0f4d78c5732f91f855981c6b6fb4f5870
jeongY <zoe1126@naver.com>
2024-05-30 12:03:47 +0900

    댓글 목록 보기 연습

--- src/main/java/com/prj2spring/mapper/comment/CommentMapper.java
+++ src/main/java/com/prj2spring/mapper/comment/CommentMapper.java
@@ -3,6 +3,9 @@ package com.prj2spring.mapper.comment;
 import com.prj2spring.domain.comment.Comment;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface CommentMapper {
@@ -15,4 +18,9 @@ public interface CommentMapper {
     int insert(Comment comment);
 
 
+    @Select("""
+            SELECT id, board_id, member_id, comment, inserted
+            FROM comment
+            """)
+    List<Comment> list(Integer boardId);
 }
